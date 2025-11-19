286b45e88787c2ce5a4cfdfb0a91467ebb8b8156
Allure Nails <244843490+allurenails6963@users.noreply.github.com>
2025-11-18 17:21:03 -0800

    New build

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,45 +1,72 @@
-import Nav from "react-bootstrap/Nav";
+
+import { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import "../Pages-CSS/Navbar.css";
 
 function DefaultTopNavBar() {
     const location = useLocation();
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const handleMenuToggle = () => setMenuOpen((open) => !open);
+    const handleMenuClose = () => setMenuOpen(false);
 
     return (
         <>
-            <div className="business-name">
-                Allure Nails & Spa
-            </div>
+            <div className="business-name">Allure Nails & Spa</div>
             <div className="navbar-top">
-                <Nav variant="underline" className="nav-right">
-                    <Nav.Item>
-                        <Nav.Link
-                            as={Link}
+                <nav className="nav-right desktop-menu">
+                    <Link
+                        to="/home"
+                        className={`nav-link${location.pathname === "/home" ? " active" : ""}`}
+                        onClick={handleMenuClose}
+                    >
+                        Home
+                    </Link>
+                    <Link
+                        to="/services"
+                        className={`nav-link${location.pathname === "/services" ? " active" : ""}`}
+                        onClick={handleMenuClose}
+                    >
+                        Services
+                    </Link>
+                    <Link
+                        to="/contact-us"
+                        className={`nav-link${location.pathname === "/contact-us" ? " active" : ""}`}
+                        onClick={handleMenuClose}
+                    >
+                        Contact Us
+                    </Link>
+                </nav>
+                <div className="mobile-menu-icon" onClick={handleMenuToggle}>
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                </div>
+                {menuOpen && (
+                    <div className="mobile-dropdown">
+                        <Link
                             to="/home"
-                            active={location.pathname === "/home"}
+                            className={`nav-link${location.pathname === "/home" ? " active" : ""}`}
+                            onClick={handleMenuClose}
                         >
                             Home
-                        </Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link
-                            as={Link}
+                        </Link>
+                        <Link
                             to="/services"
-                            active={location.pathname === "/services"}
+                            className={`nav-link${location.pathname === "/services" ? " active" : ""}`}
+                            onClick={handleMenuClose}
                         >
                             Services
-                        </Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link
-                            as={Link}
+                        </Link>
+                        <Link
                             to="/contact-us"
-                            active={location.pathname === "/contact-us"}
+                            className={`nav-link${location.pathname === "/contact-us" ? " active" : ""}`}
+                            onClick={handleMenuClose}
                         >
                             Contact Us
-                        </Nav.Link>
-                    </Nav.Item>
-                </Nav>
+                        </Link>
+                    </div>
+                )}
             </div>
         </>
     );
